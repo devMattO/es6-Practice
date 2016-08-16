685c2d0f40825c90c0c9c8402387a47e332c3291
Mattdre <olsen_matthew@yahoo.com>
2016-08-15 14:48:11 -1000

    passed 49th test

--- test/es6Practice.spec.js
+++ test/es6Practice.spec.js
@@ -389,7 +389,7 @@ describe('generator can be created in multiple ways', function() {
   it('computed generator names, are just prefixed with a `*`', function() {
     const generatorName = 'g';
     let obj = {
-      [generatorName]() {}
+      *[generatorName]() {}
     };
     assertIsGenerator(obj.g());
   });
